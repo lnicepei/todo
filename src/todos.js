000da22d360d1ce864edebd00bfc75ed303139ab
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 17:56:32 +0300

    Add functions to the todo factory function

--- src/todos.js
+++ src/todos.js
@@ -1,20 +1,12 @@
-import { updateTodoOnScreen, createTodoButton } from "./updateTodosDOM";
+import { createTodoButton } from "./updateTodosDOM";
 
 function Todo(name, description, date, priority, complete){
-    let todosArray = [];
+    
     function createTodo(){
-        alert("enter your todo");
-        let name = prompt("Name");
-        let description = prompt("Description");
-        let date = prompt("date");
-        let priority = prompt("priority");
-        let complete = 1;
-        let test = Todo(name, description, date, priority, complete);
         updateTodoOnScreen(name, description, date, priority, complete);
         createTodoButton();
-        todosArray.push(test);
-        console.log(todosArray);
     }
+    
     return{
         name: name,
         description: description,
