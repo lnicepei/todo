291a4546742fa94ff0b725d82aee50a4c3338d34
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 15:42:41 +0300

    Try to figure out, where to place functions

--- src/todos.js
+++ src/todos.js
@@ -1,14 +1,26 @@
-class Todo{
-    constructor(name, description, date, priority, complete){
-        this.name = name;
-        this.description = description;
-        this.date = date;
-        this.priority = priority;
-        this.complete = complete;
-    }
+import { updateTodoOnScreen, createTodoButton } from "./updateTodosDOM";
 
-    markComplete(){
-        this.complete = 1;
+function Todo(name, description, date, priority, complete){
+    let todosArray = [];
+    function createTodo(){
+        alert("enter your todo");
+        let name = prompt("Name");
+        let description = prompt("Description");
+        let date = prompt("date");
+        let priority = prompt("priority");
+        let complete = 1;
+        let test = Todo(name, description, date, priority, complete);
+        updateTodoOnScreen(name, description, date, priority, complete);
+        createTodoButton();
+        todosArray.push(test);
+        console.log(todosArray);
+    }
+    return{
+        name: name,
+        description: description,
+        date: date,
+        priority: priority,
+        complete: complete
     }
 }
 
