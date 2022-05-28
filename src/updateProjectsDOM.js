1c08644758878f82a4a33da44b7c47240afc63ed
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 19:39:31 +0300

    Allow change projects and looking at their content

--- src/updateProjectsDOM.js
+++ src/updateProjectsDOM.js
@@ -1,28 +1,43 @@
-import { Project } from "./projects";
+import { Project, arrayOfProjects } from "./projects";
 import { Todo } from "./todos";
 import { createTodoButton, createTodo, showTodosArray } from "./updateTodosDOM";
 let i = 1;
 
 function createProject() {
     let name = prompt('Name');
-    let testProject = new Project(name);
+    let newProject = new Project(name);
     let project = document.createElement('div');
     project.className = 'project' + i;
     i++;
-    let child = createTodo();
-    testProject.arr.push(child);
-    console.log(child);
-    console.log(testProject.arr);
-    project.addEventListener('click', showThisProject);
+    let taskInsideProject = createTodo();
+    newProject.arr.push(taskInsideProject);
+    project.addEventListener('click', () => {
+        console.log(newProject);
+        document.getElementById('content').innerHTML = '';
+        for(let i in newProject.arr){
+        document.querySelector('#content').textContent = newProject.arr[i].task.name + newProject.arr[i].task.description + newProject.arr[i].task.date + newProject.arr[i].task.priority + newProject.arr[i].task.complete;
+    }
+        createTodoButton();
+    });
     document.querySelector('.projects').appendChild(project);
     project.textContent = name;
-    createTodoButton();
+    // createTodoButton();
 }
 
-function showThisProject(){
-    document.getElementById('content').innerHTML = '';
-    showTodosArray();
-    createTodoButton();
+// function showThisProject(newProject){
+//     console.log(newProject);
+//     document.getElementById('content').innerHTML = '';
+//     for(let i in newProject.arr){
+//         document.querySelector('#content').textContent = newProject.arr[i].task.name + newProject.arr[i].task.description + newProject.arr[i].task.date + newProject.arr[i].task.priority + newProject.arr[i].task.complete;
+//     }
+//     createTodoButton();
+// }
+
+function showAllProjects(){
+    document.querySelector('.projects').innerHTML = '';
+    for(let i in arrayOfProjects){
+        document.querySelector('.projects').textContent += arrayOfProjects[i];
+    }
 }
 
 export {createProject}
