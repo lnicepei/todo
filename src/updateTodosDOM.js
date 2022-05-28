0e44249d8d707aa4ff2e271ab7aa96f1994a449e
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 17:57:14 +0300

    Create template object

--- src/updateTodosDOM.js
+++ src/updateTodosDOM.js
@@ -1,23 +1,41 @@
 import { Todo } from "./todos";
 
-function updateTodoOnScreen(){
-    let todoContainer = document.createElement('div');
-    document.querySelector('#content').appendChild(todoContainer);
-    todoContainer.textContent += test.name + test.description + test.date + test.priority + test.complete;
-}
+// function updateTodoOnScreen(){
+//     let todoContainer = document.createElement('div');
+//     document.querySelector('#content').appendChild(todoContainer);
+//     todoContainer.textContent += test.name + test.description + test.date + test.priority + test.complete;
+// }
 
 function showTodosArray(){
     for(let i in todosArray){
-        document.querySelector('#content').textContent += todosArray[i].name + ' ' + todosArray[i].description;
+        document.querySelector('#content').textContent = todosArray[i].name + todosArray[i].description + todosArray[i].date + todosArray[i].priority + todosArray[i].complete;
     }
-    console.log(todosArray);
 }
 
 function createTodoButton(){
     let createTodoButton = document.createElement('button');
     document.querySelector('#content').appendChild(createTodoButton);
     createTodoButton.textContent = 'Create todo';
-    createTodoButton.addEventListener('click', Todo.createTodo);
+    createTodoButton.addEventListener('click', createTodo);
 }
 
-export {updateTodoOnScreen, createTodoButton, showTodosArray}
+let todosArray = [];
+function createTodo(){
+    // alert("enter your todo");
+    // let name = prompt("Name");
+    // let description = prompt("Description");
+    // let date = prompt("date");
+    // let priority = prompt("priority");
+    let complete = 1;
+    let name = 'One';
+    let description = 'Descr';
+    let date = 1;
+    let priority = 0;
+    let test = Todo(name, description, date, priority, complete);
+    todosArray.push(test);
+    showTodosArray();
+    createTodoButton();
+    return{test}
+}
+
+export {createTodoButton, showTodosArray, createTodo}
