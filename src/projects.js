daf5580649f4cd541c08d94b8c4a64402830a5ab
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 15:42:14 +0300

    Import functions

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,7 @@
+import { Todo } from "./todos";
+import { createTodoButton } from "./updateTodosDOM";
+import { removeProject } from "./updateProjectsDOM";
+
 class Project{
     constructor(name){
         this.name = name;
