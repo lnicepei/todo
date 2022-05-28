054a559f1ac1e38bbb0ba1c76b379e632749616d
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 19:38:57 +0300

    Make a project push itself to arrayOfProjects

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,13 @@
+let arrayOfProjects = [];
+
 class Project{
     constructor(name){
         this.name = name;
         this.arr = [];
+        arrayOfProjects.push(this);
     }
 
-    
+    //functions to alter tasks inside a project
 }
 
-export {Project};
+export {Project, arrayOfProjects};
