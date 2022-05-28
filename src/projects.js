db7cb4b0188c44d0bf7e5b3a82ad8a5ed180445f
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 17:55:58 +0300

    Create array inside class object

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,10 @@
-import { Todo } from "./todos";
-import { createTodoButton } from "./updateTodosDOM";
-import { removeProject } from "./updateProjectsDOM";
-
 class Project{
     constructor(name){
         this.name = name;
+        this.arr = [];
     }
+
+    
 }
 
 export {Project};
