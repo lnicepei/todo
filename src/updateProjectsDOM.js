9a70155972472ba620d6d6090771e5cd86a2c047
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 17:57:04 +0300

    Create child of a project and push to array

--- src/updateProjectsDOM.js
+++ src/updateProjectsDOM.js
@@ -1,15 +1,18 @@
 import { Project } from "./projects";
 import { Todo } from "./todos";
-import { createTodoButton, showTodos, showTodosArray } from "./updateTodosDOM";
-import { createTodo } from "./updateTodosDOM";
-
+import { createTodoButton, createTodo, showTodosArray } from "./updateTodosDOM";
 let i = 1;
 
-function createProject(){
+function createProject() {
     let name = prompt('Name');
+    let testProject = new Project(name);
     let project = document.createElement('div');
     project.className = 'project' + i;
     i++;
+    let child = createTodo();
+    testProject.arr.push(child);
+    console.log(child);
+    console.log(testProject.arr);
     project.addEventListener('click', showThisProject);
     document.querySelector('.projects').appendChild(project);
     project.textContent = name;
@@ -19,6 +22,7 @@ function createProject(){
 function showThisProject(){
     document.getElementById('content').innerHTML = '';
     showTodosArray();
+    createTodoButton();
 }
 
 export {createProject}
