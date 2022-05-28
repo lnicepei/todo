b8fc928c45e3976f2edc618d31d418f180996c52
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-28 15:43:51 +0300

    Add create button function, show todos as an array

--- src/updateTodosDOM.js
+++ src/updateTodosDOM.js
@@ -1,20 +1,23 @@
-import { Project } from "./projects";
 import { Todo } from "./todos";
 
-let Inbox = new Project("Inbox");
-
-
-function createTodo(){
-    alert("enter your todo");
-    let name = prompt("Name");
-    let description = prompt("Description");
-    let date = prompt("date");
-    let priority = prompt("priority");
-    let complete = 1;
-    let test = new Todo(name, description, date, priority, complete);
+function updateTodoOnScreen(){
     let todoContainer = document.createElement('div');
     document.querySelector('#content').appendChild(todoContainer);
     todoContainer.textContent += test.name + test.description + test.date + test.priority + test.complete;
 }
 
-export {createTodo}
+function showTodosArray(){
+    for(let i in todosArray){
+        document.querySelector('#content').textContent += todosArray[i].name + ' ' + todosArray[i].description;
+    }
+    console.log(todosArray);
+}
+
+function createTodoButton(){
+    let createTodoButton = document.createElement('button');
+    document.querySelector('#content').appendChild(createTodoButton);
+    createTodoButton.textContent = 'Create todo';
+    createTodoButton.addEventListener('click', Todo.createTodo);
+}
+
+export {updateTodoOnScreen, createTodoButton, showTodosArray}
