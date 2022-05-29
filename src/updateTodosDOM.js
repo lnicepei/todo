c9664be12601d5cd7c7a2ca486b5a673790eda00
lnicepei <dmitrii.shaidorov@gmail.com>
2022-05-29 11:58:13 +0300

    Replace function related to todos

--- src/updateTodosDOM.js
+++ src/updateTodosDOM.js
@@ -1,11 +1,11 @@
 import { Todo } from "./todos";
-import { showAllTasks } from "./updateProjectsDOM"
 
 function TodoButton(newProject) {
-    let task;
     let todoButton = document.createElement('button');
+   
     document.querySelector('#content').appendChild(todoButton);
     todoButton.textContent = 'Create todo';
+
     todoButton.addEventListener('click', () => {
         let name = prompt("Name");
         // let description = prompt("Description");
@@ -15,12 +15,37 @@ function TodoButton(newProject) {
         let description = 'Description';
         let date = 1;
         let priority = 0;
-        task = Todo(name, description, date, priority, complete);
-        newProject.arr.push(task);
-        console.log(newProject);
+
+        let task = Todo(name, description, date, priority, complete);
+
+        newProject.arrayOfTodos.push(task);
         showAllTasks(newProject);
         document.querySelector('#content').appendChild(todoButton);
     });
 }
 
-export { TodoButton }
+function showAllTasks(newProject) {
+    document.getElementById('content').innerHTML = '';
+
+    for(let f = 1; f <= newProject.arrayOfTodos.length - 1; f++) {
+        let taskOnTheScreen = document.createElement('div');
+        taskOnTheScreen.className = 'task';
+
+        taskOnTheScreen.textContent += newProject.arrayOfTodos[f].name + newProject.arrayOfTodos[f].description + newProject.arrayOfTodos[f].date + newProject.arrayOfTodos[f].priority + newProject.arrayOfTodos[f].complete;
+        
+        document.querySelector('#content').appendChild(taskOnTheScreen);
+    }
+}
+
+function deleteTask(newProject){
+    for( var i = 0; i < newProject.arrayOfTodos.length; i++){ 
+    
+        if ( arr[i] === 5) { 
+    
+            newProject.arrayOfTodos.splice(i, 1); 
+        }
+    
+    }
+}
+
+export { TodoButton, deleteTask }
