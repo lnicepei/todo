8b63e38fb585ceddca0982e62b91d629dfdea10a
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-01 11:36:02 +0300

    Make adding tasks via form possible

--- src/updateTodosDOM.js
+++ src/updateTodosDOM.js
@@ -13,31 +13,38 @@ function TodoButton(newProject) {
 }
 
 function createPopup(newProject) {
-    let popup = document.querySelector('.popup');
     // popup.style.display = 'block';
+    let popup = document.querySelector('.popup');
     popup.style.transform = 'scale(1)';
-
-    document.querySelector(".submit-button").addEventListener("click", (e) => {
-        e.preventDefault();
-
-        let name = document.getElementById('taskname').value;
-        let description = document.getElementById('description').value;
-        let date = document.getElementById('date').value;
-        let priority = document.getElementById('priority').value;
-
-        let task = Todo(name, description, date, priority);
-        newProject.arrayOfTodos.push(task);
-        createTask(newProject);
-        popup.style.transform = 'scale(0)';
-
-    });
     
     document.body.addEventListener('click', function(event) {
         if (event.target == popup) {
             popup.style.transform = 'scale(0)';
-
         }
     });
+    
+    //remove this event listener
+
+    document.querySelector(".submit-button").addEventListener("click", getDataFromForm, false);
+    document.querySelector(".submit-button").parameter = newProject;
+    
+    //remove this event listener
+    
     // When the user clicks anywhere outside of the modal, close it
+}
+
+function getDataFromForm(e) {
+    
+    let name = document.getElementById('taskname').value;
+    let description = document.getElementById('description').value;
+    let date = document.getElementById('date').value;
+    let priority = document.getElementById('priority').value;
+    let task = Todo(name, description, date, priority);
+
+    let popup = document.querySelector('.popup');
+
+    e.currentTarget.parameter.arrayOfTodos.push(task);
+    popup.style.transform = 'scale(0)';
+    createTask(e.currentTarget.parameter);
 }
 export { TodoButton }
