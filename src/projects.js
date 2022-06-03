1dd32337bee3252864a3595aba6f276db73373bd
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-03 16:14:49 +0300

    Move arrayOfProjets to anothe module

--- src/projects.js
+++ src/projects.js
@@ -1,13 +1,10 @@
-let arrayOfProjects = [];
-
 class Project{
     constructor(name){
         this.name = name;
         this.arrayOfTodos = [];
-        arrayOfProjects.push(this);
     }
-
+    
     //functions to alter tasks inside a project
 }
 
-export {Project, arrayOfProjects};
+export {Project};
