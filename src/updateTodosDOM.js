4a3afa0b193be652413990dc80e759d676917fb1
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-15 15:28:30 +0300

    Blur background while making a new task

--- src/updateTodosDOM.js
+++ src/updateTodosDOM.js
@@ -6,10 +6,16 @@ function TodoButton(newProject) {
     let todoButton = document.createElement('div');
    
     document.querySelector('.create-button').appendChild(todoButton);
-    todoButton.innerHTML = `<svg style="width:50px;height:71px" viewBox="0 0 24 24">    <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>`;
+    todoButton.innerHTML = `<svg style="width:50px;height:70px" viewBox="0 0 24 24">    <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>`;
     todoButton.className = 'create-button__btn';
 
     todoButton.addEventListener('click', () => {
+        const blur = document.querySelectorAll('.blur');
+        blur.forEach((item) => {
+            item.classList.toggle('active');
+            // item.classList.remove('inactive');
+            // item.classList.add('active');
+        });
         createPopup(newProject);
     });
 }
@@ -18,13 +24,24 @@ function createPopup(newProject) {
     let popup = document.querySelector('.popup-container');
     popup.style.transform = 'scale(1)';
     
-    document.body.addEventListener('click', function(event) {
-        if (event.target == popup) {
-            popup.style.transform = 'scale(0)';
-        }
-    });
+    document.body.addEventListener('click', blurBackground);
+
     document.querySelector(".form__submit-button").addEventListener("click", getDataFromForm, false);
     document.querySelector(".form__submit-button").parameter = newProject;
+}
+
+function blurBackground(e){
+    if (e.target == document.querySelector('.popup-container')) {
+        document.querySelector('.popup-container').style.transform = 'scale(0)';
+        
+        const blur = document.querySelectorAll('.blur');
+        blur.forEach((item) => {
+            item.classList.toggle('active');
+            // item.classList.remove('active');
+            // item.classList.add('inactive');
+        });
+        // document.body.removeEventListener('click', blurBackground);
+    }
 }
 
 function getDataFromForm(e) {
@@ -37,14 +54,26 @@ function getDataFromForm(e) {
     date = format(date, 'd MMM y (EE)');
 
     if (name) {
+        document.getElementById('taskname').value = '';
+        document.getElementById('description').value = '';
+        document.getElementById('date').value = '';
+        document.getElementById('priority').value = '';
+
         if(!description) description = 'No description';
-        console.log('~ description', description);
-        let task = Todo(name, description, date, priority)
+
+        let task = Todo(name, description, date, priority);
         e.currentTarget.parameter.arrayOfTodos.push(task);
+
         localStorage.setItem('projects', JSON.stringify(arrayOfProjects));
 
         let popup = document.querySelector('.popup-container');
         popup.style.transform = 'scale(0)';
+
+        const blur = document.querySelectorAll('.blur');
+            blur.forEach((item) => {
+                // item.classList.toggle('active');
+                item.classList.remove('active');
+            });
 
         createAllTasksInProject(e.currentTarget.parameter);
     }else {
