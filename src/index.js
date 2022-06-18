494d6f76d49d1f22e0ba9361c85d4d6db5f70415
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-18 10:27:21 +0300

    Work out setting up the webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { inputProjectName } from "./updateProjectsDOM";
+import css from "./style.css"
 
 document.querySelector('.projects__btn').addEventListener('click', inputProjectName);
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 
 module.exports = {
     entry:'./src/index.js',
-    mode:'production',
+    mode:'development',
     output: {
         filename:'main.js',
         path: path.resolve(__dirname,'dist'),
+        publicPath: '/',
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        static: './dist',
+    },
 };
