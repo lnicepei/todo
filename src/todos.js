652cdd830a509a813a0172338cb7e42433a50266
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-18 13:49:28 +0300

    Prettier

--- src/todos.js
+++ src/todos.js
@@ -1,49 +1,53 @@
-import { arrayOfProjects, updateCurrentProject, createAllTasksInProject } from "./updateProjectsDOM"
-import { isAfter, isToday } from 'date-fns';
+import {
+  arrayOfProjects,
+  updateCurrentProject,
+  createAllTasksInProject,
+} from "./updateProjectsDOM";
+import { isAfter, isToday } from "date-fns";
 
-function Todo(name, description, date, priority, project){
-    return{
-        name: name,
-        description: description,
-        date: date,
-        priority: priority,
-        project: project
-    }
+function Todo(name, description, date, priority, project) {
+  return {
+    name: name,
+    description: description,
+    date: date,
+    priority: priority,
+    project: project,
+  };
 }
 
 function createUpcomingTasks() {
-    document.querySelector('.content').textContent = '';
-    document.querySelector('.create-button').textContent = '';
+  document.querySelector(".content").textContent = "";
+  document.querySelector(".create-button").textContent = "";
 
-    updateCurrentProject('Upcoming');
+  updateCurrentProject("Upcoming");
 
-    arrayOfProjects.forEach(project => {
-        for(let f in project.arrayOfTodos) {
-            if(isAfter(new Date(project.arrayOfTodos[f].date), new Date())){
-                createAllTasksInProject(project, f, 'upcoming');
-            }
-        }
-    });
+  arrayOfProjects.forEach((project) => {
+    for (let f in project.arrayOfTodos) {
+      if (isAfter(new Date(project.arrayOfTodos[f].date), new Date())) {
+        createAllTasksInProject(project, f, "upcoming");
+      }
+    }
+  });
 }
 
 function createTodaysTasks() {
-    document.querySelector('.content').textContent = '';
-    document.querySelector('.create-button').textContent = '';
-    
-    updateCurrentProject('Today');
+  document.querySelector(".content").textContent = "";
+  document.querySelector(".create-button").textContent = "";
 
-    arrayOfProjects.forEach(project => {
-        for (let f in project.arrayOfTodos) {
-            if(isToday(new Date(project.arrayOfTodos[f].date))) {
-                createAllTasksInProject(project, f);
-            }
-        }
-    });
+  updateCurrentProject("Today");
+
+  arrayOfProjects.forEach((project) => {
+    for (let f in project.arrayOfTodos) {
+      if (isToday(new Date(project.arrayOfTodos[f].date))) {
+        createAllTasksInProject(project, f);
+      }
+    }
+  });
 }
 
 function deleteTask(newProject, numberOfTask) {
-    newProject.arrayOfTodos.splice(numberOfTask, 1); 
-    localStorage.setItem('projects', JSON.stringify(arrayOfProjects));
+  newProject.arrayOfTodos.splice(numberOfTask, 1);
+  localStorage.setItem("projects", JSON.stringify(arrayOfProjects));
 }
 
-export {Todo, createUpcomingTasks, createTodaysTasks, deleteTask};
+export { Todo, createUpcomingTasks, createTodaysTasks, deleteTask };
